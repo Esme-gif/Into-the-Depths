74c94bae4d95b7be6f9e14dd4f91b3d7c53613f7
ArianiBobani <amertz@uci.edu>
2021-05-19 16:52:22 -0700

    Setting up audio for movement
    Added basic movement scripts and sound effects

---
+++ Assets/EliasAudioPlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliasAudioPlay : MonoBehaviour
+{
+    public AudioSource src;
+    public AudioClip [] steps;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        src = GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayStep() {
+        src.clip = steps[Random.Range(0,0)];
+        src.PlayOneShot(steps[0]);
+    }
+}
