903ec081f11661b4d9cc12f1bf3f2fcd664ca374
Arya V <71158876+Aryariart@users.noreply.github.com>
2021-10-02 21:53:45 -0400

    Level/rat - 10/2/21
    Expanded upon the level, increased speed on the rats, tried to create cutscene trigger

---
+++ Assets/intro_cutscene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intro_cutscene : MonoBehaviour
+{
+    public bool triggered;
+    public PlayerScript player;
+    public GameObject cutscenetrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered)
+        {
+
+        }
+    }
+}
